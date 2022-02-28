98231b0f24796e43462f664465afcd7c7d538814
Filip Janješić <83244348+Filip-Janjesic@users.noreply.github.com>
2022-03-01 00:09:00 +0100

    Update farmmeeting.sql

--- farmmeeting.sql
+++ farmmeeting.sql
@@ -1,7 +1,7 @@
 drop database if exists farmmeeting;
 create database farmmeeting character set utf8 collate utf8_general_ci;
 use farmmeeting ;
-
+-----------------------------------------------------------------------------------------------------------------------
 create table farmers(
 	id int not null primary key auto_increment,
 	opg varchar(50)	not null,
@@ -27,7 +27,20 @@ create table farmers_meeting(
 	meeting int not null,
 	farmers int not null
 );
-
+-----------------------------------------------------------------------------------------------------------------------
 alter table farmers_meeting add foreign key (farmers) references farmers(id);
 alter table farmers_meeting add foreign key (meeting) references meeting(id);
 alter table meeting add foreign key (farmType) references farmType(id);
+-----------------------------------------------------------------------------------------------------------------------
+insert into farmers (id, opg, email, contactphone) values
+(null, 'Admin', 'admin@email.com', 0911111111),
+(null, 'OPG1', 'opg1@email.com', 0911111112),
+(null, 'OPG2', 'opg2@email.com', 0911111113),
+(null, 'OPG3', 'opg3@email.com', 0911111114),
+(null, 'OPG4', 'opg4@email.com', 0911111115),
+(null, 'OPG5', 'opg5@email.com', 0911111116),
+(null, 'OPG6', 'opg6@email.com', 0911111117),
+(null, 'OPG7', 'opg7@email.com', 0911111119),
+(null, 'OPG8', 'opg8@email.com', 0912211110),
+(null, 'OPG9', 'opg9@email.com', 0913311121),
+(null, 'OPG10','opg10@email.com',09184411122);
