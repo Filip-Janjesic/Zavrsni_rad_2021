079a1cdaaf31372603b4cf317487f7c5394a424b
Filip Janješić <83244348+Filip-Janjesic@users.noreply.github.com>
2022-03-01 00:21:48 +0100

    Update farmmeeting.sql

--- farmmeeting.sql
+++ farmmeeting.sql
@@ -33,16 +33,21 @@ alter table farmers_meeting add foreign key (meeting) references meeting(id);
 alter table meeting add foreign key (farmType) references farmType(id);
 
 insert into farmers (id, opg, email, contactphone) values
-(null, 'Admin', 'admin@email.com', 0911111111),
-(null, 'OPG1', 'opg1@email.com', 0911111112),
-(null, 'OPG2', 'opg2@email.com', 0911111113),
-(null, 'OPG3', 'opg3@email.com', 0911111114),
-(null, 'OPG4', 'opg4@email.com', 0911111115),
-(null, 'OPG5', 'opg5@email.com', 0911111116),
-(null, 'OPG6', 'opg6@email.com', 0911111117),
-(null, 'OPG7', 'opg7@email.com', 0911111119),
-(null, 'OPG8', 'opg8@email.com', 0912211110),
-(null, 'OPG9', 'opg9@email.com', 0913311121),
-(null, 'OPG10','opg10@email.com',09184411122);
+(null, 'Admin', 'admin@email.com', 091111111),
+(null, 'OPG1', 'opg1@email.com', 091111112),
+(null, 'OPG2', 'opg2@email.com', 091111113),
+(null, 'OPG3', 'opg3@email.com', 091111114),
+(null, 'OPG4', 'opg4@email.com', 091111115),
+(null, 'OPG5', 'opg5@email.com', 091111116),
+(null, 'OPG6', 'opg6@email.com', 091111117),
+(null, 'OPG7', 'opg7@email.com', 091111119),
+(null, 'OPG8', 'opg8@email.com', 091221110),
+(null, 'OPG9', 'opg9@email.com', 091331121),
+(null, 'OPG10','opg10@email.com',0918441122);
 
-
+insert into farmType (id, name, numOfWorkers) values
+(null, 'Poultry farming', 5),
+(null, 'Pig farming', 8),
+(null, 'Cattle farming', 15),
+(null, 'Beekeeping', 2),
+(null, 'Fruit growing', 4);
