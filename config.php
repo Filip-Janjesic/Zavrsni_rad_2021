0d3a28f351524b0e8dac68fc996423d98aa6ee17
Filip Janješić <83244348+Filip-Janjesic@users.noreply.github.com>
2022-03-01 23:08:07 +0100

    config

---
+++ config.php
@@ -0,0 +1,24 @@
+<?php
+
+    error_reporting(0);
+    $user = "afrodita_Filip-J"; // Mysql user
+    $pass = "t84durft1994"; // Mysql pass
+    $db = new PDO("mysql:dbname=afrodita_Zavrsni_rad_2021;host=localhost;charset=utf8", $user, 'afrodita_Filip-J');
+
+class Request
+{
+    public static function pathInfo()
+    {
+        if (isset($_SERVER['PATH_INFO'])) {
+            return $_SERVER['PATH_INFO'];
+        } elseif (isset($_SERVER['REDIRECT_PATH_INFO'])) {
+            return $_SERVER['REDIRECT_PATH_INFO'];
+        } else {
+            return '';
+        }
+    }
+    public static function post($key, $default = '')
+    {
+        return isset($_POST[$key]) ? $_POST[$key] : $default;
+    }
+}
