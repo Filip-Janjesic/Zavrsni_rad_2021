5a5ea73c9e8fe4a32cb7b673d8af4d672f8025f1
Filip Janješić <83244348+Filip-Janjesic@users.noreply.github.com>
2022-03-06 13:32:38 +0100

    update

--- main/newfarmtype.php
+++ main/newfarmtype.php
@@ -25,3 +25,29 @@
     $term->execute();
     $list=$term->fetchAll();
 ?>
+
+<div class="jumbotron jumbotron-sm">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12 col-lg-12">
+                <h1 class="h1">
+                    <small>New farm type</small>
+                </h1>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container">
+    <div class="row">
+        <div class=" col-lg-4 col-sm-12"></div>
+        <div class=" col-lg-4 col-sm-12">
+            <h4 style="padding-left: 2cm; padding-bottom: 1cm">Create new farm type</h4>
+            <form action="" method="post" name="update">
+                <input class="form-control" style="text-align: center;margin-bottom: 1cm" type="datetime-local" name="matchStart" placeholder="Odaberi datum">
+                <input class="form-control" style="text-align: center;margin-bottom: 1cm" type="text" name="matchLocation" placeholder="Upiši lokaciju">
+                <input class="form-control" style="text-align: center;margin-bottom: 1cm" type="text" name="matchPrice" placeholder="Upiši cijenu">
+                <button class="btn btn-success btn-block" type="submit"><i class="fas fa-sign-in-alt"></i>Spremi promjene</button>
+            </form>
+        </div>
+    </div>
