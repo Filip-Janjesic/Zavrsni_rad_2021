ce96e0d3b08d83a1952419162e31f28c2965b993
Filip Janješić <83244348+Filip-Janjesic@users.noreply.github.com>
2022-03-06 23:48:08 +0100

    Update arrangednewmeeting.php

--- main/arrangednewmeeting.php
+++ main/arrangednewmeeting.php
@@ -59,6 +59,7 @@
                 <th scope="col">Start date</th>
                 <th scope="col">Location</th>
                 <th scope="col">Reason</th>
+                <th scope="col">Delete</th>
             </tr>
         </thead>
         <tbody>
