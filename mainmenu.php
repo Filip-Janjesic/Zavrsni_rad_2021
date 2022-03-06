5047ea402e6260ed213c81500a531eb2fedc7cd9
Filip Janješić <83244348+Filip-Janjesic@users.noreply.github.com>
2022-03-06 16:51:44 +0100

    fix2

--- mainmenu.php
+++ mainmenu.php
@@ -54,7 +54,7 @@
                 <?php
                         if (!isset($_GET['x']) || $_GET['x'] == 1) { include("main/homepage.php"); }
                         else if ($_GET['x'] == 2) { include("main/arrangedmeetings.php"); }
-                        else if ($_GET['x'] == 4) { include("main/createnewfarmtype.php"); }
+                        else if ($_GET['x'] == 4) { include("main/newfarmtype.php"); }
                         else if ($_GET['x'] == 5) { include("main/settings.php"); }
                         else if ($_GET['x'] == 6) { include("main/aboutus.php"); }
                         else if ($_GET['x'] == 7) { include("main/contact.php"); }
