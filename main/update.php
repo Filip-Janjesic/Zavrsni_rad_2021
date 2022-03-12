c909e509cf8e71b72bf8dc0f922bfcce8e83c04f
Filip Janješić <83244348+Filip-Janjesic@users.noreply.github.com>
2022-03-12 16:57:18 +0100

    Update update.php

--- main/update.php
+++ main/update.php
@@ -3,8 +3,8 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-    include_once "./head.php";
-    include_once "./config.php";
+    include_once "head.php";
+    include_once "config.php";
 
     $term = $db->prepare("SELECT * FROM farmType");
     $term->execute();
