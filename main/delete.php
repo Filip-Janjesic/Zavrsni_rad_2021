eeb4c2f7e318080c6ed8fc9454eaf9c64724e1ba
Filip Janješić <83244348+Filip-Janjesic@users.noreply.github.com>
2022-03-13 22:38:03 +0100

    delete1

--- main/delete.php
+++ main/delete.php
@@ -2,16 +2,13 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-
-
-$user = "afrodita_Filip-J"; // Mysql user
-$pass = "t84durft1994"; // Mysql pass
-$db = new PDO("mysql:dbname=afrodita_Zavrsni_rad_2021;host=localhost;charset=utf8", $user, 't84durft1994');
+include_once "../config.php";
 
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-  $sql = "DELETE FROM meeting WHERE id=:id";
-
+  $id=(int)
+  $sql = "DELETE FROM meeting WHERE id =".$id;
+  $db = exec($sql);
   echo "Record deleted successfully";
 
 $conn = null;
