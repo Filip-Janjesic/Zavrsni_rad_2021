7b4c3ad391c589eab86fb425e2fb004ae62104e5
Filip Janješić <83244348+Filip-Janjesic@users.noreply.github.com>
2022-03-15 19:50:59 +0100

    Update delete.php

--- main/delete.php
+++ main/delete.php
@@ -1,17 +1,11 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 include_once "../config.php";
 
 try {
   $db = new PDO("mysql:dbname=afrodita_Zavrsni_rad_2021;host=localhost;charset=utf8", $user, $pass);
   
   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-  $sql = "DELETE FROM meeting WHERE id=$id";
-
+  $sql = "DELETE FROM meeting WHERE id=:id";
   $db->exec($sql);
 
   echo "Meeting has been terminated";
