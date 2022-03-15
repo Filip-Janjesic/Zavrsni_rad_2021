4b9f589feee355a10567023bd9f6f65a2b0ba879
Filip Janješić <83244348+Filip-Janjesic@users.noreply.github.com>
2022-03-15 19:27:41 +0100

    Update delete.php

--- main/delete.php
+++ main/delete.php
@@ -1,5 +1,28 @@
 <?php
-include_once "config.php";
+error_reporting(0);
+$user = "afrodita_Filip-J"; // Mysql user
+$pass = "t84durft1994"; // Mysql pass
+$db = new PDO("mysql:dbname=afrodita_Zavrsni_rad_2021;host=localhost;charset=utf8", $user, $pass);
+
+class Request
+{
+public static function pathInfo()
+{
+    if (isset($_SERVER['PATH_INFO'])) 
+    {
+        return $_SERVER['PATH_INFO'];
+    } elseif (isset($_SERVER['REDIRECT_PATH_INFO'])) 
+    {
+        return $_SERVER['REDIRECT_PATH_INFO'];
+    } else {
+        return '';
+    }
+}
+public static function post($key, $default = '')
+{
+    return isset($_POST[$key]) ? $_POST[$key] : $default;
+}
+}
 
 try {
   $db = new PDO("mysql:dbname=afrodita_Zavrsni_rad_2021;host=localhost;charset=utf8", $user, $pass);
