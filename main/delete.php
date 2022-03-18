cc9253ee667938e971b1e785b034c14e7bc4ed99
Filip Janješić <83244348+Filip-Janjesic@users.noreply.github.com>
2022-03-18 21:35:42 +0100

    Update delete.php

--- main/delete.php
+++ main/delete.php
@@ -3,12 +3,11 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-
 include_once "../config.php";
 
 if(isset($_GET["id"])) 
 {
-    $result = mysql_query("DELETE FROM 'meeting' WHERE id=".$_GET['id']);
+    $result = mysql_query("DELETE FROM 'meeting' WHERE id=", intval($_GET['id']));
     
     if(!$result){
       echo mysql_error();
