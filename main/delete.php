f457a02ab81ef4d764e6d1f201a44ef47e63b734
Filip Janješić <83244348+Filip-Janjesic@users.noreply.github.com>
2022-03-19 18:00:59 +0100

    Update delete.php

--- main/delete.php
+++ main/delete.php
@@ -2,19 +2,3 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-
-include ('../config.php');
-
-$id = $_GET['id'];
-
-$stmt = "DELETE FROM meeting WHERE id = :id";
-$stmt->bindParam(':id', $id);
-
-// Delete
-if(isset($GET['btn_delete'])) { 
-  $id = $_GET['id']; 
-  $stmt->execute();
-  header('Location: arrangednewmeeting.php');
-}
-
-?>
