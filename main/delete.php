446533e495699e91a511c43d8aa99dde1e25d04f
Filip Janješić <83244348+Filip-Janjesic@users.noreply.github.com>
2022-03-22 17:17:45 +0100

    Update delete.php

--- main/delete.php
+++ main/delete.php
@@ -2,3 +2,14 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+
+
+if(isset($_GET['id'])) {
+    $result = mysql_query("DELETE FROM `meeting` WHERE id='".mysql_real_escape_string($_GET['id']). "'");
+    echo mysql_error();
+    if($result)
+        echo "succces";
+
+} else {
+    echo 'GET NOT SET';
+}
