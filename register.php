42e90264faf01af416c71a2468ab6869ce0b2692
Filip Janješić <83244348+Filip-Janjesic@users.noreply.github.com>
2022-03-28 18:24:49 +0200

    Update register.php

--- register.php
+++ register.php
@@ -3,13 +3,15 @@
     include_once "config.php";
     session_start();
 
-    $term = $db->prepare("INSERT INTO farmers (opg, email, contactphone) VALUES
+    $term = $db->prepare("INSERT INTO farmers (opg, email, password,contactphone) VALUES
     (:opg, :email, :contactphone)");
     $term->bindValue('opg', Request::post("opg"));
     $term->bindValue('email', Request::post("email"));
+    $term->bindValue('password', Request::post("password"));
     $term->bindValue('contactphone', Request::post("contactphone"));
     $term->execute();
 
     $_SESSION["email"]=Request::post("email");
+    $_SESSION["password"]=Request::post("password");
     include_once "mainmenu.php";
 ?>
