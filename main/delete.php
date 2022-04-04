a8bbada504fe190ee09ce83a61ac05927a3cfef8
Filip Janješić <83244348+Filip-Janjesic@users.noreply.github.com>
2022-04-04 21:33:13 +0200

    Update delete.php

--- main/delete.php
+++ main/delete.php
@@ -4,8 +4,6 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 include_once "../config.php";
 
-
-
 try {
     $db = new PDO("mysql:dbname=afrodita_Zavrsni_rad_2021;host=localhost;charset=utf8", $user, $pass);
 
