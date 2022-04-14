cdaa90e80b286981a2eda7097f61e4171f5a1fdf
Filip Janješić <83244348+Filip-Janjesic@users.noreply.github.com>
2022-04-14 15:19:13 +0200

    errors

--- config.php
+++ config.php
@@ -1,4 +1,10 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+?>
+
+<?php
 
     error_reporting(0);
     $user = "afrodita_Filip-J"; // Mysql user
