76fbe18903be20bf09223b44592ee30adcbc81ac
Filip Janješić <83244348+Filip-Janjesic@users.noreply.github.com>
2022-04-26 20:14:41 +0200

    error overview

--- head.php
+++ head.php
@@ -1,3 +1,9 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+?>
+
 <meta charset="UTF-8">
 <meta name="viewport"
       content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
