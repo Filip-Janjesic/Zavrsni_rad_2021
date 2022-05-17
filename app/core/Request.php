6342d99f9fb3ba86290ebf16279a8c5b6035cf43
Filip Janješić <83244348+Filip-Janjesic@users.noreply.github.com>
2022-05-17 17:31:46 +0200

    update request

--- app/core/Request.php
+++ app/core/Request.php
@@ -1,1 +1,28 @@
-1
+<?php
+
+class Request
+{
+    public static function getRoute()
+    {
+        if(isset($_SERVER['REDIRECT_URL'])){
+            $Route = $_SERVER['REDIRECT_URL'];
+        }elseif(isset($_SERVER['REDIRECTION_PATH_INFO']))
+        {
+            $Route = $_SERVER['REDIRECTION_PATH_INFO'];
+        }elseif(isset($_SERVER['REQUEST_URI']))
+        {
+            $Route = $_SERVER['REQUEST_URI'];
+        }
+        return $Route;
+    }
+
+    public static function isLogin()
+    {
+        return isset($_SESSION['User']);
+    }
+
+    public static function isAdmin()
+    {
+        if(isset($_SESSION))
+    }
+}
