9729c9816bc12600327527eeec414dd2ce6791f8
Filip Janješić <83244348+Filip-Janjesic@users.noreply.github.com>
2022-05-28 20:55:30 +0200

    footer

---
+++ app/view/templatePieces/footer.php
@@ -0,0 +1,46 @@
+<br><br>
+<footer class="text-center text-lg-start bg-light text-muted">
+  <section class="">
+    <div class="container text-center text-md-start mt-5">
+      <div class="row mt-3">
+        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+          <h6 class="text-uppercase fw-bold mb-4">
+            <i class="fas fa-gem me-3"></i><?= App::config('siteTitle'); ?>
+          </h6>
+          <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </div>
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+          <h6 class="text-uppercase fw-bold mb-4">
+            Useful links
+          </h6>
+          <p>
+            <a href="<?= App::config('url'); ?>index/index" class="text-reset">Buy</a>
+          </p>
+          <p>
+            <a href="<?=App::config('url'); ?>index/contact" class="text-reset">Contact</a>
+          </p>
+          <p>
+            <a href="<?=App::config('url'); ?>login" class="text-reset">Login</a>
+          </p>
+          <p>
+            <a href="<?=App::config('url'); ?>/login/register" class="text-reset">Register</a>
+          </p>
+        </div>
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+          <h6 class="text-uppercase fw-bold mb-4">
+            Contact
+          </h6>
+          <p><i class="fas fa-home me-3"></i> Višnjevac, 31220, HR</p>
+          <p>
+            <i class="fas fa-envelope me-3"></i>
+            filip.janjesic@gmail.com
+          </p>
+          <p><i class="fas fa-phone me-3"></i> +38591234567</p>
+          <p><i class="fas fa-print me-3"></i> +38591234567</p>
+        </div>
+      </div>
+    </div>
+  </section>
+</footer>
