02ff7d0fa42049fa1c3dc591094d0993060692fc
Filip Janješić <83244348+Filip-Janjesic@users.noreply.github.com>
2022-05-28 20:22:55 +0200

    template

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # Zavrsni_rad-Farm-Shop
-Final project for Edunova PHP 2021
---
+++ app/view/template.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include 'templatePieces/header.phtml'?>
+</head>
+<body>
+    <?php include 'templatePieces/navigation.phtml'?>
+    <?= $content ?>
+    <?php include 'templatePieces/footer.phtml'?>
+    <?php include 'templatePieces/script.phtml'?>
+</body>
+</html>
