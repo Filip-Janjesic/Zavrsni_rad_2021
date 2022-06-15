98c0f0b87bae2561c6655bb3c505c611f6cdc015
Filip Janješić <83244348+Filip-Janjesic@users.noreply.github.com>
2022-06-15 18:14:40 +0200

    Create AuthorizationController.php

---
+++ app/controller/AuthorizationController.php
@@ -0,0 +1,23 @@
+<?php
+
+
+class AuthorizationController extends Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+        if(!isset($_SESSION['User'])){
+            $this -> view -> render('login/login',[
+                'errors' => [
+                    'email' => '',
+                    'password' => '',
+                    'alert' => 'For this content you must be logged in!'],
+                'returnField' => [
+                    'email' => '',
+                    'password' => '']
+            ]);
+            exit;
+        }
+    }
+}
